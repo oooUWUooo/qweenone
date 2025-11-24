6eb1d9e71074c42aeddb0ca857d1679eaa8818d7
oooUWUooo <ooo.youcatchmela+1@gmail.com>
2025-11-24 16:47:56 +0600

    Create init.py

---
+++ src/enhanced_agents/init.py
@@ -0,0 +1,11 @@
+# Modern Agent System with Cognitive Architectures
+from .enhanced_agent_system import EnhancedAgentSystem, CognitiveAgent
+from .crewai_integration import CrewAIAgentOrchestrator
+from .langchain_enhanced_agents import LangChainAgentWrapper
+
+__all__ = [
+    "EnhancedAgentSystem", 
+    "CognitiveAgent", 
+    "CrewAIAgentOrchestrator",
+    "LangChainAgentWrapper"
+]
